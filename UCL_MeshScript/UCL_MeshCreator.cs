9e37979180ed2dac9244e6c38f0bd567f204aa74
tim099 <tim11251994@gmail.com>
2020-05-13 17:41:50 +0800

    Update UCL_MeshCreator.cs

--- UCL_MeshScript/UCL_MeshCreator.cs
+++ UCL_MeshScript/UCL_MeshCreator.cs
@@ -8,13 +8,16 @@ namespace UCL.MeshLib {
     public class UCL_MeshCreator : MonoBehaviour {
         public UnityEngine.Rendering.IndexFormat m_IndexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
 
-        public List<Vector3> m_Vertices = new List<Vector3>();
-        public List<int> m_Triangles = new List<int>();
-        public List<Vector2> m_UV = new List<Vector2>();
+        public bool f_InitOnStart = true;
 
         protected MeshFilter m_MeshFilter;
         protected MeshRenderer m_MeshRenderer;
         protected Mesh m_Mesh;
+
+        void Start() {
+            if(f_InitOnStart) Init();
+        }
+
         virtual public void Init() {
             m_MeshRenderer = GetComponent<MeshRenderer>();
             if(m_MeshRenderer == null) {
@@ -31,39 +34,11 @@ namespace UCL.MeshLib {
             m_Mesh = m_MeshFilter.mesh;
             m_Mesh.indexFormat = m_IndexFormat;
             m_Mesh.Clear();
-
-            using(UCL_IntArray arr = new UCL_IntArray(10)) {
-                Debug.LogWarning("arr");
-            }
-            m_Mesh.subMeshCount = 2;
-            Debug.LogWarning("arr2");
-            m_Mesh.vertices = m_Vertices.ToArray();
-            //m_Mesh.SetTriangles(m_Triangles.ToArray(), 0,true, 0); //SetTriangles(int[] triangles, int trianglesStart
-            var tri_arr = m_Triangles.ToArray();
-            m_Mesh.SetTriangles(tri_arr, 0,3, 0);
-            m_Mesh.SetTriangles(tri_arr, 3,3, 1);
-            //m_Mesh.triangles = m_Triangles.ToArray();
-            //SetTriangles(m_Triangles.ToArray());
-            m_Mesh.uv = m_UV.ToArray(); // add this line to the code here
-            m_Mesh.Optimize();
-            m_Mesh.RecalculateNormals();
+        }
+        virtual public void GenerateMesh() {
 
         }
-        /*
-        virtual public void SetTriangles(System.Array arr) {
-            //SetTrianglesImpl
-            var SetTrianglesImpl = typeof(Mesh).GetMethod("SetTrianglesImpl", BindingFlags.NonPublic | BindingFlags.Instance);
-            if(SetTrianglesImpl != null) {
-                SetTrianglesImpl?.Invoke(m_Mesh, new object[] { -1, UnityEngine.Rendering.IndexFormat.UInt32, arr, arr.Length, 0, arr.Length, true, 0 });
-            } else {
-                Debug.LogError("SetTrianglesImpl Not Found!!");
-            }
-            //m_Mesh.triangles = arr;
-        }
-        */
-        void Start() {
-            Init();
-        }
+
 
         // Update is called once per frame
         void Update() {
---
+++ UCL_MeshScript/UCL_MeshCreatorBasic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UCL.MeshLib {
+    public class UCL_MeshCreatorBasic : UCL_MeshCreator {
+        public List<Vector3> m_Vertices = new List<Vector3>();
+        public List<int> m_Triangles = new List<int>();
+        public List<Vector2> m_UV = new List<Vector2>();
+        public override void Init() {
+            base.Init();
+            GenerateMesh();
+        }
+        override public void GenerateMesh() {
+            m_Mesh.subMeshCount = 2;
+            Debug.LogWarning("arr2");
+            m_Mesh.vertices = m_Vertices.ToArray();
+            //m_Mesh.SetTriangles(m_Triangles.ToArray(), 0,true, 0); //SetTriangles(int[] triangles, int trianglesStart
+            var tri_arr = m_Triangles.ToArray();
+            m_Mesh.SetTriangles(tri_arr, 0, 3, 0);
+            m_Mesh.SetTriangles(tri_arr, 3, 3, 1);
+            //m_Mesh.triangles = m_Triangles.ToArray();
+            //SetTriangles(m_Triangles.ToArray());
+            m_Mesh.uv = m_UV.ToArray(); // add this line to the code here
+            m_Mesh.Optimize();
+            m_Mesh.RecalculateNormals();
+        }
+    }
+}
